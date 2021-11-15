062afb0ab5bd5e8f8c5789435cdc6a86da28a698
KwelonD <94276151+KwelonD@users.noreply.github.com>
2021-11-14 18:15:05 -0600

    1st HTML/CSS Project

---
+++ Tech-Academy.html/Tech-Academy.html/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tech_Academy.html
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
